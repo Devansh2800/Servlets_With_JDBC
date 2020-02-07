e4262876aa67748a706763cdf6db10e424aa6c47
Devansh Devagya <42578725+Devansh2800@users.noreply.github.com>
2020-02-07 14:46:08 +0530

    Consist of adding record to the database whose columns are predifined.
    Database used is HeidiSQL .

---
+++ AddRecordServlet.java
@@ -0,0 +1,43 @@
+import javax.servlet.*;
+import java.io.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class AddRecordServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException
+	{
+	PrintWriter out = res.getWriter();
+	res.setContentType("text/html");
+	try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost/iactdata?user=root&password=");
+			
+			int eno = Integer.parseInt(req.getParameter("n1"));
+
+			String sn = req.getParameter("t1");
+
+			int sal = Integer.parseInt(req.getParameter("n2"));
+			
+			String dn = req.getParameter("t2");
+
+			
+			String qry = "insert into emp values(?,?,?,?)";
+			PreparedStatement ps = con.prepareStatement(qry);
+			ps.setInt(1,eno);
+			ps.setString(2,sn);
+			ps.setInt(3,sal);
+			ps.setString(4,dn);
+			int a = ps.executeUpdate();
+			if(a==1){
+				out.println("Data is recorded");
+			}
+			else{
+				out.println("Data is not recorded");
+				
+			}
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
